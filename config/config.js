a4d85a3d98dc4c9b647cd8fd5468802f7aa4ed26
lijinqiang@ppmoney.com <lijinqiang@ppmoney.com>
2020-01-06 14:31:08 +0800

    fix: 保存文件

---
+++ config/config.js
@@ -0,0 +1,6 @@
+const path = require('path')
+
+module.exports = {
+  assetsRoot: path.resolve(__dirname, '../dist'),
+  assetsDirectory: 'static',
+}
---
+++ index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default class App extends React.Compoment {
+
+    render() {
+      return <div>...there is app index</div>
+    }
+}
